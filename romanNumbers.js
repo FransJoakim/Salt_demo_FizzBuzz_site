afefaec2f134fcb5f747c034b01e2b83a19ffa84
SumanaSattar <sumana.sattar@gmail.com>
2022-01-28 12:16:43 +0100

    final demo

--- romanNumbers.js
+++ romanNumbers.js
@@ -1,13 +1,43 @@
 const converterButton = document.querySelector("#arabic-roman-button");
 const inputArabic = document.querySelector("#input-arabic-number");
 const inputRoman = document.querySelector("#input-roman-number");
+const inputlabel = document.querySelector("#input-label");
+const label = document.querySelector("#label");
+const arrow = document.querySelector(".arrowChange");
+const tilte = document.querySelector("#title-arabic-roman")
+let arabicNum = true;
+let romanNum =false;
+arrow.addEventListener('click', e => {
+     arabicNum=!arabicNum;
+     romanNum=!romanNum;
+     inputArabic.value = '';
+     inputRoman.value = '';
+     if(arabicNum === true) {
+       inputlabel.textContent = "Arabic Number";
+       label.textContent = "Roman Number";
+       tilte.textContent = "Arabic Converter"
+     }else {
+      inputlabel.textContent = "Roman Number";
+      label.textContent = "Arabic Number";
+      tilte.textContent = "Roman Converter"
+     }
+
+})
 
 converterButton.addEventListener("click", (e) => {
   e.preventDefault();
-  const arabicNum = inputArabic.value;
-  const result = toRomanNumbers(arabicNum);
-  inputRoman.value = result;
-  console.log(result)
+  if(arabicNum == true) {
+    const arabicNum = inputArabic.value;
+    const result = toRomanNumbers(arabicNum);
+    inputRoman.value = result;
+    console.log(result)
+  } else  {
+    const romanNum = inputArabic.value;
+    const result = toArabicNumbers(romanNum);
+    inputRoman.value = result;
+    console.log(result)
+  }
+  
 });
 
 function toRomanNumbers(input) {
@@ -40,3 +70,25 @@ function toRomanNumbers(input) {
   }   
   
 }
+
+function toArabicNumbers(input) {
+  const romanNumbers = [[1,'I'],[5, 'V'], [10,'X'],[50,'L'],[100,'C'],[500,'D'],[1000,'M']];
+  
+  const charArr = input.split('');
+  const reverseArr = charArr.reverse();
+  let result = 0;
+  let lastDigit = 0;
+  reverseArr.forEach( element => {
+      const arabicNumm = romanNumbers.find(o => o[1]=== element);
+
+      if(lastDigit <= arabicNumm[0]) {
+          result += arabicNumm[0];
+        } else {
+          result -= arabicNumm[0];
+        }
+        lastDigit = arabicNumm[0];
+  })
+  
+  return result;
+}
+
